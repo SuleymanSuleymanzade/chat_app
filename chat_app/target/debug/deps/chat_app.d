2a4afc6743d03f6c69b23b4a1f2a195873ca4598
Suleyman Suleymanzade <sul_soft@hotmail.com>
2023-03-08 20:55:24 +0400

    message app

--- chat_app/target/debug/deps/chat_app.d
+++ chat_app/target/debug/deps/chat_app.d
@@ -3,3 +3,5 @@ C:\Users\sul_s\Documents\rust_web_projects\chat_app\chat_app\target\debug\deps\c
 C:\Users\sul_s\Documents\rust_web_projects\chat_app\chat_app\target\debug\deps\chat_app.d: src\main.rs
 
 src\main.rs:
+
+# env-dep:CARGO_MANIFEST_DIR=C:\\Users\\sul_s\\Documents\\rust_web_projects\\chat_app\\chat_app
